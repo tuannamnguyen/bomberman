c8a8ea267b2e09445ac0b3d14612e2caaa300db5
tuannamnguyen <tuannamnguyen290602@gmail.com>
2021-12-01 00:06:55 +0700

    minor change

--- uet/oop/bomberman/entities/movingObj/Bomber.java
+++ uet/oop/bomberman/entities/movingObj/Bomber.java
@@ -16,7 +16,7 @@ public class Bomber extends movingObjects {
 
     private facingDirection direction;
 
-    private boolean isAlive = true;
+    public static boolean isAlive = true;
 
     public Bomber(int x, int y, Image img) {
         super(x, y, img);
@@ -25,62 +25,13 @@ public class Bomber extends movingObjects {
     @Override
     public void update() {
         if (BombermanGame.currentlyPressedKey.contains(KeyCode.D)) {
-            this.setX(this.getX() + speed);
-
-            List<Image> imgList = new ArrayList<>();
-            imgList.add(Sprite.player_right.getFxImage());
-            imgList.add(Sprite.player_right_1.getFxImage());
-            imgList.add(Sprite.player_right_2.getFxImage());
-
-            AnimatedImage player = new AnimatedImage(imgList, 0.100);
-
-            double t = (System.nanoTime() - BombermanGame.startNanoTime) / 1000000000.0;
-            this.img = player.getFrame(t);
-            direction = facingDirection.RIGHT;
-
+            moveRight();
         } else if (BombermanGame.currentlyPressedKey.contains(KeyCode.A)) {
-            this.setX(this.getX() - speed);
-
-            List<Image> imgList = new ArrayList<>();
-            imgList.add(Sprite.player_left.getFxImage());
-            imgList.add(Sprite.player_left_1.getFxImage());
-            imgList.add(Sprite.player_left_2.getFxImage());
-
-            AnimatedImage player = new AnimatedImage(imgList, 0.100);
-
-            double t = (System.nanoTime() - BombermanGame.startNanoTime) / 1000000000.0;
-            this.img = player.getFrame(t);
-            direction = facingDirection.LEFT;
-
+            moveLeft();
         } else if (BombermanGame.currentlyPressedKey.contains(KeyCode.W)) {
-            this.setY(this.getY() - speed);
-
-            List<Image> imgList = new ArrayList<>();
-            imgList.add(Sprite.player_up.getFxImage());
-            imgList.add(Sprite.player_up_1.getFxImage());
-            imgList.add(Sprite.player_up_2.getFxImage());
-
-            AnimatedImage player = new AnimatedImage(imgList, 0.100);
-
-            double t = (System.nanoTime() - BombermanGame.startNanoTime) / 1000000000.0;
-            this.img = player.getFrame(t);
-            direction = facingDirection.UP;
-
+            moveUp();
         } else if (BombermanGame.currentlyPressedKey.contains(KeyCode.S)) {
-
-            this.setY(this.getY() + speed);
-
-            List<Image> imgList = new ArrayList<>();
-            imgList.add(Sprite.player_down.getFxImage());
-            imgList.add(Sprite.player_down_1.getFxImage());
-            imgList.add(Sprite.player_down_2.getFxImage());
-
-            AnimatedImage player = new AnimatedImage(imgList, 0.100);
-
-            double t = (System.nanoTime() - BombermanGame.startNanoTime) / 1000000000.0;
-            this.img = player.getFrame(t);
-            direction = facingDirection.DOWN;
-
+            moveDown();
         } else {
             if (direction == facingDirection.UP) {
                 this.img = Sprite.player_up.getFxImage();
@@ -94,17 +45,69 @@ public class Bomber extends movingObjects {
         }
     }
 
-    public boolean isAlive() {
-        return isAlive;
-    }
-
-    public void setAlive(boolean isAlive) {
-        this.isAlive = isAlive;
-    }
-
     @Override
     public boolean canMove() {
-        
+
     }
-    
+
+    private void moveUp() {
+        this.setY(this.getY() - speed);
+
+        List<Image> imgList = new ArrayList<>();
+        imgList.add(Sprite.player_up.getFxImage());
+        imgList.add(Sprite.player_up_1.getFxImage());
+        imgList.add(Sprite.player_up_2.getFxImage());
+
+        AnimatedImage player = new AnimatedImage(imgList, 0.100);
+
+        double t = (System.nanoTime() - BombermanGame.startNanoTime) / 1000000000.0;
+        this.img = player.getFrame(t);
+        direction = facingDirection.UP;
+    }
+
+    private void moveDown() {
+        this.setY(this.getY() + speed);
+
+        List<Image> imgList = new ArrayList<>();
+        imgList.add(Sprite.player_down.getFxImage());
+        imgList.add(Sprite.player_down_1.getFxImage());
+        imgList.add(Sprite.player_down_2.getFxImage());
+
+        AnimatedImage player = new AnimatedImage(imgList, 0.100);
+
+        double t = (System.nanoTime() - BombermanGame.startNanoTime) / 1000000000.0;
+        this.img = player.getFrame(t);
+        direction = facingDirection.DOWN;
+
+    }
+
+    private void moveLeft() {
+        this.setX(this.getX() - speed);
+
+        List<Image> imgList = new ArrayList<>();
+        imgList.add(Sprite.player_left.getFxImage());
+        imgList.add(Sprite.player_left_1.getFxImage());
+        imgList.add(Sprite.player_left_2.getFxImage());
+
+        AnimatedImage player = new AnimatedImage(imgList, 0.100);
+
+        double t = (System.nanoTime() - BombermanGame.startNanoTime) / 1000000000.0;
+        this.img = player.getFrame(t);
+        direction = facingDirection.LEFT;
+    }
+
+    private void moveRight() {
+        this.setX(this.getX() + speed);
+
+        List<Image> imgList = new ArrayList<>();
+        imgList.add(Sprite.player_right.getFxImage());
+        imgList.add(Sprite.player_right_1.getFxImage());
+        imgList.add(Sprite.player_right_2.getFxImage());
+
+        AnimatedImage player = new AnimatedImage(imgList, 0.100);
+
+        double t = (System.nanoTime() - BombermanGame.startNanoTime) / 1000000000.0;
+        this.img = player.getFrame(t);
+        direction = facingDirection.RIGHT;
+    }
 }
